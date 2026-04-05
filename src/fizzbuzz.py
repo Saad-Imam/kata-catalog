980f5c0c6d6c60e20ba9483950bc5514712d5ae5
Abdul Wasay Imran <abdulwasay905@gmail.com>
2026-04-05 15:01:41 +0500

    Implementedrgfbwrstyhnbywr

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -17,7 +17,19 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzzHelper(4), '4')
 
 def fizzbuzzHelper(n):
-    return ""
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        print(fizzbuzzHelper(i))
 
 if __name__ == '__main__':
     unittest.main()
+    fizzbuzz(10000000000000000000000000)
